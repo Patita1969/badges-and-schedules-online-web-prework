cb4c3b85a9481d3b5078b2cc71562671e0804b2c
delightful-tree-6762 <mduckworth@gmail.com>
2019-03-04 02:25:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,3 +18,13 @@ def assign_rooms(speakers)
     }
    return assign
 end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+  
+  assign_rooms(attendees).each do |badge|
+    puts badge
+  end
+end 
