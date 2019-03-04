3264d787104173b9b8da567d78c0ddb5436f5420
delightful-tree-6762 <mduckworth@gmail.com>
2019-03-04 02:41:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,28 +4,27 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-
-def batch_badge_creator(attendees)
-  badges = []
-  badges.push("Hello, my name is #{badge}.")
+def batch_badge_creator(names)
+  greetings = [] # initialize greetings as an empty array
+  names.each do |name| # for each name in the names array
+    greetings <<  badge_maker(name)# add a greeting for that name
+  end
+  return greetings # return the array of all greetings, at the end
 end
-return badges
 
 def assign_rooms(speakers)
-  assign=[]
-
-  speakers.each_with_index{|speaker,index|
-  assign.push("Hello, #{speaker}! You'll be assigned to room #{index + 1}!")
-    }
-   return assign
-end
-
-def printer(attendees)
-  batch_badge_creator(attendees).each do |badge|
-    puts badge
+  greet = []
+  speakers.each_with_index{ |speakers, index| greet << "Hello, #{speakers}! You'll be assigned to room #{index+1}!"}
+  return greet
   end
 
-  assign_rooms(attendees).each do |badge|
-    puts badge
+def printer(attendees)
+  resultOne = batch_badge_creator(attendees)
+  resultOne.each do |x|
+    puts x 
+  end
+  result = assign_rooms(attendees)
+  result.each do |x|
+    puts x 
   end
 end
